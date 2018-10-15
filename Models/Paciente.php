e61d6127159bab42cadfe4ea5f78db93db730d85
Wladimir Rocha <wld.rocha@gmail.com>
2018-10-14 20:52:53 -0400

    datos Finales

--- Models/Paciente.php
+++ Models/Paciente.php
@@ -26,7 +26,7 @@
         }
         public function add(){
 
-            $sql = "SELECT id_paciente WHERE nombre = '{$this->cedula}'";
+            $sql = "SELECT id_paciente WHERE cedula = '{$this->cedula}'";
             if ($this->conexion->consultaRetorno($sql)->num_rows > 0){
                 $msj ="El Paciente que introdujo ya existe, por tanto no se introdujo";
             }else{
@@ -52,8 +52,36 @@
 
         }
         public function edit(){
-            
+            $sql = "SELECT id FROM paciente WHERE cedula = '{$this->cedula}'    
+					&& id !='{$this->id}'";
+				if ($datos = $this->conexion->consultaRetorno($sql)->num_rows > 0){
+					$msj = "El Número de cedula ya se encuentra registrado";
+					
+				}else{
+					$sql = "UPDATE paciente SET nombre ='{$this->nombre}', apellido = '{$this->apellido}', cedula ='{$this->cedula}', diagnostico ='{$this->diagnostico}'
+						WHERE id='{$this->id}'";
+					if($this->conexion->consultaSimple($sql))
+					{
+						$msj = "Paciente editado Satisfactoriamente!";
+						$alert = "success";
+					}
+					else
+					{
+						$msj = "No se pudo editar el paciente";
+						$alert = "danger";
+					}
+					if($alert== null){
+						$datos['alert'] = "danger";
+					}
+					if(isset($msj)){
+					$datos = array();
+					$datos['alert'] = $alert;
+					$datos['msj'] = $msj;
+					}
+                    return $datos;
+                }
         }
+
         public function delete(){
             $sql = "DELETE FROM paciente WHERE id = '{$this->id}'";
 				if($this->conexion->consultaSimple($sql)){
